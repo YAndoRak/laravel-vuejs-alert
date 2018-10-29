49b59c59e722907fdfd649e14463982113f530b1
ANDRIANAVALONA Ando <rakotofiringa.ando.andrianavalona@esti.mg>
2018-10-29 10:48:21 +0100

    Mise en place de l'Api pour la page article

--- resources/assets/js/components/ArticleComponent.vue
+++ resources/assets/js/components/ArticleComponent.vue
@@ -5,3 +5,23 @@
         <p>{{articles.description}}</p>
     </div>
 </template>
+<script>
+    export default {
+        data(){
+            return {
+                articles:[]
+            }
+        },
+        mounted(){
+
+            axios.get('/api/post/'+this.$route.params.id).then((response)=>{
+                if(response.data!=''){
+                    this.articles=response.data
+                }
+                console.log('success')
+            }).catch(()=>{
+                console.log('error')
+            })
+        }
+    }
+</script>

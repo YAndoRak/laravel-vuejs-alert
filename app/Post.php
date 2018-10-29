7f308ed6c654671e38ac65224d15b78db84b1a0a
ANDRIANAVALONA Ando <rakotofiringa.ando.andrianavalona@esti.mg>
2018-10-29 20:20:24 +0100

    Ajout scopePublic pour filtrer les articles en public

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable=['titre', 'lieu', 'commune', 'date', 'description', 'online'];
+    public function scopePublic($req){
+        return $req->where('online', true);
+    }
 }

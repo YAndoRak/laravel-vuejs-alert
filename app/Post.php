738a646255c12d77beff39eb1e187e2b0719820e
ANDRIANAVALONA Ando <rakotofiringa.ando.andrianavalona@esti.mg>
2018-11-18 10:00:06 +0100

    Modification du model Post pour rendre fillable les nouveaux champ

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable=['titre', 'lieu', 'commune', 'date', 'description', 'online'];
+    protected $fillable=['titre', 'lieu', 'commune', 'date', 'poster', 'image', 'description', 'online'];
     public function scopePublic($req){
         return $req->where('online', true);
     }

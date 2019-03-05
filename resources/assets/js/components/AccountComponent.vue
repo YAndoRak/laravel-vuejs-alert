8e94e779d97dc25b06abc51c1aff5f939b1ce036
ANDRIANAVALONA Ando <rakotofiringa.ando.andrianavalona@esti.mg>
2019-03-05 14:44:33 +0300

    Amelioration du fonctionnalite modification d'email

--- resources/assets/js/components/AccountComponent.vue
+++ resources/assets/js/components/AccountComponent.vue
@@ -7,12 +7,21 @@
                     <div class="card-body">
                         <div class="mb-2">{{user.id}}</div>
                         <div v-if="name" class="mb-2"> {{getUser.name}}<a @click="name=false"> Edit</a></div>
-                        <div v-else class="mb-2"> <input type="text" v-model.lazy="getUser.name" /><button @click="confirmName()">Confirmer</button></div>
+                        <div v-else class="mb-2">
+                            <input type="text" v-model.lazy="getUser.name" />
+                            <button @click="confirmName()">Confirmer</button>
+                        </div>
                         <div v-if="email" class="mb-2"> {{getUser.email}}<a @click="email=false"> Edit</a></div>
-                        <div v-else class="mb-2"> <input type="text" v-model.lazy="getUser.email" /><button @click="confirmEmail()">Confirmer</button></div>
+                        <div v-else class="mb-2">
+                            <input type="text" v-model.lazy="getUser.email" />
+                            <button @click="confirmEmail()">Confirmer</button>
+                            <div v-if="alert" class="alert alert-warning mt-2" role="alert">
+                                Attention, l'adresse email existe deja ou invalide
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <button class="btn btn-primary btn-block btn-lg mt-4" @click="save()">Sauvegarder</button>
+                <button class="btn btn-primary btn-block btn-lg mt-4" @click="save()" v-bind:disabled="alert">Sauvegarder</button>
             </div>
         </div>
     </div>
@@ -22,25 +31,34 @@
         data(){
             return{
                 getUser:[],
+                getCompare:'',
                 user:{
                     name:'',
                     email:''
                 },
                 name:true,
-                email:true
+                email:true,
+                alert:false
             }
         },
         mounted(){
             axios.get('/api/user').then((response)=>{
                 this.getUser=response.data
+                this.getCompare=response.data.email
             }).catch(()=>{
                 console.log('error')
             })
         },
         methods:{
             confirmEmail(){
-                this.user.email=this.getUser.email
-                this.email=true
+                if (this.getCompare===this.getUser.email){
+                    this.alert=true
+                    this.user.email=''
+                }else{
+                    this.user.email=this.getUser.email
+                    this.alert=false
+                    this.email=true
+                }
             },
             confirmName(){
                 this.user.name=this.getUser.name
